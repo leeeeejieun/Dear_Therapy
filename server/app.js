b9f693669a43cd9b5e5373f2abc3cad807bbd5e6
leeeeejieun <arjleun19@gmail.com>
2024-10-08 01:18:20 +0900

    [INIT] 초기 세팅

---
+++ server/app.js
@@ -0,0 +1,20 @@
+// 모듈
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const app = express();
+dotenv.config();
+
+// 미들웨어
+app.use(cors({
+    origin: "http://localhost:3000", // 클라이언트 도메인
+    credentials: true // 쿠키와 같은 인증 정보를 허용
+}));
+app.use(cookieParser());                     
+app.use(express.json());     
+app.use(express.urlencoded({ extended: false }));  
+
+
+module.exports = app;
---
+++ server/bin/www.js
@@ -0,0 +1,6 @@
+const PORT = process.env.PORT || 4000;
+const app = require("../app");
+
+app.listen(PORT , ()=>{
+    console.log(`${PORT}번 포트로 서버 가동 중`);
+});
