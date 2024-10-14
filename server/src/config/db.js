a0e97dea44c2365051aa516b7a4b770f7acc679d
leeeeejieun <arjleun19@gmail.com>
2024-10-15 02:36:16 +0900

    커넥션 풀 생성 및  AWS RDS MySQL 서버 연동

---
+++ server/src/config/db.js
@@ -0,0 +1,23 @@
+const mysql = require("mysql2/promise");
+
+// 커넥션 풀 생성
+const pool = mysql.createPool({
+    connectionLimit: 10, 
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_DATABASE
+});
+
+
+// query 모듈화
+module.exports = {
+    connection: async(query, values = []) => {
+        try {
+            const [result] = await pool.query(query, values);
+            return result[0];
+        }catch(err) {
+            return err;
+        }
+    }
+}
