1171fa8951b75b63a28d3641552c7a0864917397
leeeeejieun <arjleun19@gmail.com>
2024-10-14 20:26:02 +0900

    [FEAT] 콘솔에서 HTTP 상태 로그 확인을 위한 morgan 구현

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 dotenv.config();
@@ -12,9 +13,9 @@ app.use(cors({
     origin: "http://localhost:3000", // 클라이언트 도메인
     credentials: true // 쿠키와 같은 인증 정보를 허용
 }));
+app.use(morgan('dev'));
 app.use(cookieParser());                     
 app.use(express.json());     
 app.use(express.urlencoded({ extended: false }));  
-
 
 module.exports = app;
