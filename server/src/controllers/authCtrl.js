e7903e178ff831203ca151304ab9b68d996be9c0
leeeeejieun <arjleun19@gmail.com>
2024-10-17 01:46:25 +0900

    [FEAT] 로그인 API 컨트롤러에 JWT를 처리하는 로직을 추가
    - accessToken을 JSON 형식으로 반환
    - refreshToken을 쿠키에 저장하여 반환

--- server/src/controllers/authCtrl.js
+++ server/src/controllers/authCtrl.js
@@ -2,15 +2,24 @@ const Auth = require("../models/authModel");
 const responseUtils = require("../utils/responseUtils"); 
 
 const authCtrl = {
-        login: async (req, res) => {
-           try {
-                const auth = new Auth(req.body);
-                const response = await auth.login();
-                responseUtils.createResponse(res, response);
-           } catch(err) {
-                responseUtils.createResponse(res, {code: 500});
-           };
-        },
+     login: async (req, res) => {
+          try {
+               const auth = new Auth(req.body);
+               const response = await auth.login();
+               
+               if(response.code === 200) {
+                    // RefreshToken을 쿠키로 넘김
+                    res.cookie("refreshToken", response.refreshToken, {
+                         httpOnly: true,
+                         secure: true,
+                         maxAge: 60 * 60 * 24 * 14,   // 유효 기간을 14일로 설정
+                    });
+               }
+               responseUtils.createResponse(res, response);
+          } catch(err) {
+               responseUtils.createResponse(res, {code: 500});
+          };
+     },
 };
 
 module.exports = authCtrl;
