33ff4c02048fbdce468cd968e574e319409551a8
leeeeejieun <arjleun19@gmail.com>
2024-10-18 11:10:24 +0900

    [FEAT] accessToken 재발급을 위한 API 컨트롤러 및 라우터 추가
    - POST /tokens 경로를 통해 refreshToken을 사용하여 accessToken 재발급 처리
    - refreshToken은 쿠키를 통해 자동으로 전달 받아 처리

--- server/src/controllers/authCtrl.js
+++ server/src/controllers/authCtrl.js
@@ -12,7 +12,7 @@ const authCtrl = {
                     res.cookie("refreshToken", response.refreshToken, {
                          httpOnly: true,
                          secure: true,
-                         maxAge: 60 * 60 * 24 * 14,   // 유효 기간을 14일로 설정
+                         maxAge: 60 * 60 * 24 * 14,   // 쿠키의 유효 기간을 14일로 설정
                     });
                }
                responseUtils.createResponse(res, response);
@@ -20,6 +20,17 @@ const authCtrl = {
                responseUtils.createResponse(res, {code: 500});
           };
      },
+     refresh: async (req, res) => {
+          // 헤더에서 refreshToken 추출
+          const refreshToken = req.cookies.refreshToken;  
+          try {
+               const auth = new Auth(refreshToken);
+               const response = await auth.refresh();
+               responseUtils.createResponse(res, response);
+          }catch(err) {
+               responseUtils.createResponse(res, {code: 500});
+          };
+     }
 };
 
 module.exports = authCtrl;
