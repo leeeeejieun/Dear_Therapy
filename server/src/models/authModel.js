353a9f67793483c036dff314c179b418180fa3fe
leeeeejieun <arjleun19@gmail.com>
2024-10-18 11:04:45 +0900

    [FEAT] refreshToken을 사용한 accessToken 재발급 로직 추가
    - accessToken 만료 시, 유효한 refreshToken을 통해 새로운 accessToken 발급

--- server/src/models/authModel.js
+++ server/src/models/authModel.js
@@ -16,7 +16,7 @@ class Auth {
             const {user_id, password} = findUser;             
             if(client.user_id === user_id && client.password === password) {  // 입력한 아이디와 비밀번호가 모두 일치한 경우
                 const accessToken = jwtUtils.sign({user_id: user_id}); 
-                const refreshToken = jwtUtils.sign({}, {expiresIn: "14d"});
+                const refreshToken = jwtUtils.sign({}, {expiresIn: "14d"});   // 유효기간을 2주로 설정
                 
                 await authStorage.insertRefresh(user_id, refreshToken);
                 return { code: 200, data: {accessToken : accessToken }, refreshToken};
@@ -24,8 +24,29 @@ class Auth {
         }
         // 아이디가 존재하지 않거나, 아이디 또는 비밀번호가 일치하지 않은 경우
         return { code: 401, message: "아이디 또는 비밀번호가 잘못되었습니다." }
-    }
-};
+    };
+    
+    // accessToken 만료 시 refreshToken을 이용하여 재발급 수행
+    async refresh() {
+        const refreshToken = this.body;
+
+        if (!refreshToken) {
+            return { code: 401, message: "refreshToken가 존재하지 않습니다." };
+        }
+    
+        const findUser = await authStorage.findUser({ refresh_token: refreshToken });
+        const result = findUser ? jwtUtils.verify(refreshToken) : false;
+       
+        // refreshToken이 유효한 경우 accessToken을 재발급
+        if (result.ok) {
+            const { user_id } = findUser;
+            const newAccessToken = jwtUtils.sign({ user_id: user_id });
+            return { code: 200, data: {newAccessToken: newAccessToken }};
+        }
+    
+        return { code: 401, message: "refreshToken이 유효하지 않습니다." };
+    };
+}
 
 module.exports = Auth;
 
