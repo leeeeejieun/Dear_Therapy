0701a123ff7bf7a2112c338e09ee272dbfece0d9
이경윤 <two0627@naver.com>
2024-10-20 01:18:17 +0900

    [FIX] 쿼리 오류 수정

--- server/src/models/diaryStorage.js
+++ server/src/models/diaryStorage.js
@@ -1,20 +1,13 @@
-"use strict";
-
 const db = require("../config/db");
 
 class DiaryStorage{
   //생성된 일기를 DB에 저장
   static async createDiary(diaryInfo){
-    try{
-      const {userId, title, content, imagePath} = diaryInfo;
-      const query = "INSERT INTO diary (userId, title, content, imagePath, createdAt) VALUES (?, ?, ?, ?, NOW())";
-      await db.connection.query(query, [userId, title, content, imagePath]);
-      console.log("성공적으로 저장됐습니다.");
-    } catch(err){
-      console.error("일기 저장 중 오류 발생:", err);
-      throw err;
-    }
+    const {user_id, title, content, imagePath} = diaryInfo;
 
+    const query = "INSERT INTO Diary (user_id, title, content, image) VALUES (?, ?, ?, ?)";
+    console.log(diaryInfo);
+    await db.connection(query, [user_id, title, content, imagePath]);
   }
 }
 
