86c8c964adc423f547172222b27bf8463e61925e
이경윤 <two0627@naver.com>
2024-10-20 01:13:51 +0900

    [FIX] 날짜 형식 오류 응답 로직 추가

--- server/src/controllers/diaryCtrl.js
+++ server/src/controllers/diaryCtrl.js
@@ -2,20 +2,22 @@ const Diary = require("../models/diaryModel");
 const responseUtils = require("../utils/responseUtils");
 
 const diaryCtrl = {
-  process: {
     create: async (req, res) => {
       try{
         const post = new Diary(req.body, req.file || null);
         const response = await post.create();
-        responseUtils.createResponse(res, { code: 500 });
+
+        if (response.code === 400 && response.message) {
+          return responseUtils.createResponse(res, {code: 400}); 
+        }
+
+        responseUtils.createResponse(res, response );
 
 
       } catch(err){
-        responseUtils.createResponse(res, resoonse);
+        responseUtils.createResponse(res,{code: 500} );
       };
     }
-  }
-
-};
+  };
 
 module.exports = diaryCtrl;
