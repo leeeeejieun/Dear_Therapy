6b24a708a1d19fba6c21ace5706d6e55f88275bb
이경윤 <two0627@naver.com>
2024-10-19 19:04:07 +0900

    [FIX] createResponse 메서드를 통해 응답 메시지를 생성

--- server/src/controllers/diaryCtrl.js
+++ server/src/controllers/diaryCtrl.js
@@ -1,18 +1,18 @@
 const Diary = require("../models/diaryModel");
-const s3Util = require('../utils/s3Util');
 const responseUtils = require("../utils/responseUtils");
 
 const diaryCtrl = {
   process: {
     create: async (req, res) => {
       try{
-        const post = new Diary(req.body, req.file);
+        const post = new Diary(req.body, req.file || null);
         const response = await post.create();
-        res.status(response.code).end();
+        responseUtils.createResponse(res, response);
+
 
       } catch(err){
-        console.error(err); //발생한 에러를 콘솔에 로그로 기록
-        res.status(500).json({message:"오류가 발생했습니다."});
+        console.error("Error occurred:", err);
+        responseUtils.createResponse(res, { code: 500, message: "서버 오류 발생" });
       };
     }
   }
