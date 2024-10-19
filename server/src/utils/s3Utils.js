9eca7160063cbf255757542007a504f40bb905cb
이경윤 <two0627@naver.com>
2024-10-20 01:22:50 +0900

    [FIX] 변수명 변경 및 날짜 데이터 형식 변경

--- server/src/utils/s3Utils.js
+++ server/src/utils/s3Utils.js
@@ -2,6 +2,7 @@ const {PutObjectCommand, GetObjectCommand} = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 const multer = require('multer');
 
+
 const s3 = require('../config/s3');
 const bucket =  process.env.BUCKET;
 
@@ -10,10 +11,9 @@ module.exports = {
     upload : multer({ storage: multer.memoryStorage() }),  // 메모리에 이미지 파일 임시 저장
 
     // s3에 사용자 이미지 저장
-    uploadImage: async (baseFolder, image, userId) => {
-        const date = new Date().toISOString().split('T')[0]; // 오늘 날짜를 'YYYY-MM-DD' 형식으로 가져오기
+    uploadImage: async (baseFolder, image, user_id, date) => {
         const fileName = Buffer.from(image.originalname, 'utf8').toString('utf8'); // 한글 깨짐 방지
-        const path = `${baseFolder}/${userId}/${fileName}_${date}`; // 날짜와 파일명을 포함한 경로
+        const path = `${baseFolder}/${user_id}/${fileName}_${date}`; // 날짜와 파일명을 포함한 경로
         
         const command =  new PutObjectCommand({
             Bucket: bucket,
