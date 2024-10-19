a3602823ae8c684479cb63cbfe89cf6b6d506894
이경윤 <two0627@naver.com>
2024-10-20 01:17:12 +0900

    [FIX] 날짜 형식 유효성 검사 로직 추가 및 코드 수정

--- server/src/models/diaryModel.js
+++ server/src/models/diaryModel.js
@@ -1,46 +1,43 @@
-"use strict";
 
 const s3Utils = require('../utils/s3Utils');
 const diaryStorage = require('./diaryStorage');
+const responseUtils = require('../utils/responseUtils');
 
 class Diary{
-  constructor(userId, title, content){
-    this.userId = userId;
-    this.title = title;
-    this.content = content;
-    this.image = image;
+  constructor(body, file){
+        this.body = body;
+        this.file = file; 
+        this.date = body.date;
+      }
     
-  }
+      static isValidDate(dateString) {
+        const regex = /^\d{4}-\d{2}-\d{2}$/; // YYYY-MM-DD 형식 정규 표현식
+        return regex.test(dateString);
+      }
+    
 
   async create() {
-    try {
-      const { userId, title, content, image } = this.body; 
-      //본문이 없는 경우
-      if (!content || content.trim() === "") {
-        return { code: 400, message: "내용을 입력하지 않았습니다." };
-      }
+    const { user_id, title, content } = this.body;
+    const image = this.file;  
 
-      let imagePath = null;
+    // content가 없는 경우
+    if (!content || content.trim() === "") {
+     return {code: 400} 
+    }
+    //날짜형식이 잘못된 경우
+    if (this.date && !Diary.isValidDate(this.date)) {
+      return { code: 400, message: "날짜 형식이 잘못되었습니다." };
+    }
 
-      // 이미지가 존재할 경우 S3에 업로드
-      if (image) {
-        imagePath = await s3Util.uploadImage('diary_images', image, userId); // S3에 이미지 업로드 후 경로 반환
-      }
-
-      // 다이어리 정보 저장
-      const diaryInfo = {
-        userId,
-        title,
-        content,
-        imagePath 
-      };
-
-      await DiaryStorage.createDiary(diaryInfo); 
-
-      return { code: 200 }; // 성공 응답
-    } catch (err) {
-      throw err;
-   }
+    const imagePath = image ? await s3Utils.uploadImage("diary_images", image, user_id) : null; 
+    const diaryInfo = { user_id, title, content, imagePath, date: this.date };
+ 
+    await diaryStorage.createDiary(diaryInfo); 
+    
+    return { code: 201 };
   }
 }
+
   module.exports = Diary;
+
+  //날짜형식 유효성 검사 후 리턴문에 적기
