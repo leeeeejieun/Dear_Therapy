f8304716794ecadc0755f463a9d0a7ea3043f1e4
이경윤 <two0627@naver.com>
2024-10-28 01:25:08 +0900

    [FIX] DB에 일기 조회 기능 추가

--- server/src/models/diaryStorage.js
+++ server/src/models/diaryStorage.js
@@ -1,13 +1,20 @@
 const db = require("../config/db");
 
 class DiaryStorage{
+
+  // DB에 이미 존재하는 일기 조회
+  static async findDiaryByDate(user_id, date) {
+    const query = "SELECT * FROM Diary WHERE user_id = ? AND created_date = ?";
+    const result = await db.connection(query, [user_id, date]);
+    return result; // 결과 반환 (없으면 null)
+}
   //생성된 일기를 DB에 저장
   static async createDiary(diaryInfo){
-    const {user_id, title, content, imagePath} = diaryInfo;
+    const {user_id, title, content, imagePath, created_date} = diaryInfo;
 
-    const query = "INSERT INTO Diary (user_id, title, content, image) VALUES (?, ?, ?, ?)";
+    const query = "INSERT INTO Diary (user_id, title, content, image, created_date) VALUES (?, ?, ?, ?, ?)";
    
-    await db.connection(query, [user_id, title, content, imagePath]);
+    await db.connection(query, [user_id, title, content, imagePath, created_date]);
   }
 }
 
