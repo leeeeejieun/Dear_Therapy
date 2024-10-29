c3e03254710736662bd0355aec73c49f25446bcc
leeeeejieun <arjleun19@gmail.com>
2024-10-29 22:55:51 +0900

    [REFACTOR] Title 코드 리팩토링
    - 시작 페이지와, 로그인 페이지에 공통으로 사용되는 Titel 컴포넌트 구현

---
+++ client/src/components/common/Title.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = () => {
+    return (
+        <TitleContainer>
+            <TitleText>Dear Therapist</TitleText>
+        </TitleContainer>
+    );
+};
+
+export default Title;
+
+const TitleContainer = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 250px;
+    height: 170px;
+    background: url("https://diary-project-images.s3.ap-northeast-2.amazonaws.com/frontend/ob1.png") no-repeat center center; 
+    background-size: cover;
+`;
+
+const TitleText = styled.h1`
+    position: absolute;
+    font-size: 45px;
+    color: ${(props) => props.theme.text};
+    transform: translateY(-30%);
+    letter-spacing: -1px;
+    font-family: '빵구니맘', sans-serif;
+    z-index: 1; 
+`;
