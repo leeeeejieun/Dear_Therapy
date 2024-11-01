e5a8002e1d04b75ad8fbbbb80b4667cbfff2b82e
leeeeejieun <arjleun19@gmail.com>
2024-11-02 02:48:55 +0900

    [FEAT] OpenAI를 사용한 감정 점수 측정 로직 구현
    - parseFloat 메서드를 사용하여 응답에서 숫자 데이터만 반환하도록 하고, 이를 통해 숫자가 아닌 데이터의 응답을 차단
    - 점수를 1~10점 범위로 측정하여 반환

--- server/src/utils/commentUtils.js
+++ server/src/utils/commentUtils.js
@@ -4,23 +4,41 @@ const client = new OpenAI({
     api_key: process.env.OPENAI_API_KEY
 });
 
-// 코멘트 생성 함수
-const createComment = async (diaryContent) => {
-    const response = await client.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        max_tokens: 256,
-        messages: [
-        {
-          role: "system",
-          content: process.env.ASSISTANT_ROLE
-        },
-        {
-          role: "user",
-          content: diaryContent
-        }
-      ]
-    }); 
-    return response.choices[0].message.content;
-}
-
-module.exports = createComment;
+module.exports  = { 
+    // 코멘트 생성 함수
+    createComment : async (diaryContent) => {
+      const response = await client.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          max_tokens: 256,
+          messages: [
+          {
+            role: "system",
+            content: process.env.ASSISTANT_ROLE_1
+          },
+          {
+            role: "user",
+            content: diaryContent
+          }
+        ]
+      }); 
+      return response.choices[0].message.content;
+    },
+    // 코멘트 생성 함수
+    createSentiment: async (diaryContent) => {
+      const response = await client.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          max_tokens: 256,
+          messages: [
+          {
+            role: "system",
+            content: process.env.ASSISTANT_ROLE_2
+          },
+          {
+            role: "user",
+            content: diaryContent
+          }
+        ]
+      }); 
+      return parseFloat(response.choices[0].message.content);
+    }
+};
