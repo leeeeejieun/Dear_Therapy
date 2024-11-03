ff08f6d5d33b7c53dd60f8777b39eaafb3907c96
이경윤 <two0627@naver.com>
2024-11-04 00:03:32 +0900

    [FIX] 일기조회 API 라우팅 추가

--- server/src/routes/diary.js
+++ server/src/routes/diary.js
@@ -10,5 +10,7 @@ const diaryCtrl = require("../controllers/diaryCtrl");
 const storage = multer.memoryStorage(); //메모리에 파일 저장
 const upload = multer({storage: storage}); //multer 설정
 router.post("/", upload.single("image"), diaryCtrl.create );
+router.get('/:user_id/:date', diaryCtrl.getDiaryByDate);
+
 
 module.exports = router;
