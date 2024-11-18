aaa68d6c18133e5cd57f5bd41675dfd8001045e0
이경윤 <two0627@naver.com>
2024-11-18 18:23:10 +0900

    [FIX] 잘못된 형태일 경우 에러 로직 추가

--- server/src/models/diaryModel.js
+++ server/src/models/diaryModel.js
@@ -1,10 +1,11 @@
 const s3Utils = require('../utils/s3Utils');
-const diaryStorage = require('./diaryStorage');
+const diaryStorage = require('../models/diaryStorage');
 
 class Diary{
   constructor(body, file){
         this.body = body;
         this.file = file; 
+        this.user_id = body.user_id ? body.user_id.trim() : ""; 
         this.date = body.date ? body.date.trim() : ""; // date에 공백이 있을 경우 빈 문자열로 처리하며 undifined인 경우 공백으로 뜨도록 설정
       }
     
@@ -29,7 +30,7 @@ class Diary{
     }
 
     // 해당 날짜에 이미 일기가 존재하는 경우
-    const existingDiary = await diaryStorage.findDiaryByDate(user_id, this.date); 
+    const existingDiary = await diaryStorage.findDate(user_id, this.date); 
     if (existingDiary) {
       return { code: 409, message: "해당 날짜의 일기가 이미 존재합니다." };
     }
@@ -43,14 +44,21 @@ class Diary{
     
     return { code: 201 };
   }
-
+  
   // 특정 사용자의 특정 날짜에 해당하는 일기 조회
-  static async findDiaryByUserAndDate(user_id, date) {
-    const diary = await diaryStorage.findDiaryByDate(user_id, date);  
+  async lookUp() { 
     
-    // 일기가 없는 경우 처리
-    if (!diary) {
-      return { code: 404, message: "해당 날짜의 일기가 존재하지 않습니다." }; }
+    const diary = await diaryStorage.findDate(this.user_id, this.date); // findDate 호출
+    
+    // user_id나 date가 공백이거나 undefined인 경우
+    if (!this.user_id || this.user_id.trim() === "" || !this.date || this.date.trim() === "") {
+      return { code: 400, message: "잘못된 형태의 데이터 입니다." }; 
+}
+      
+    
+      // 일기가 없는 경우 처리
+      if (!diary) {
+        return { code: 404, message: "해당 날짜의 일기가 존재하지 않습니다." }; }
 
       
     // 일기가 존재할 경우 이미지 URL 생성
@@ -58,11 +66,27 @@ class Diary{
      const signedUrl = await s3Utils.getImage(diary.image);
      diary.imageUrl = signedUrl;
     }
-
-    return { code: 200, data: diary };
     
+   
+    
+    return { 
+      code: 200, 
+      data: {
+        diary: { 
+            title: diary.title, 
+            content: diary.content, 
+            image: diary.image || null
+          },
+          recommendation: { 
+            comment: null 
+          },
+        },
+        
+      }
+    
+    }
   }
 
-}
+
 
   module.exports = Diary;
