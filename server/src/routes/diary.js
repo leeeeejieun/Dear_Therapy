06524694c07f932332fc28f255f6b6f4cdbfa67c
이경윤 <two0627@naver.com>
2024-11-22 22:49:58 +0900

    [FIX] JWT 미들웨어 추가

--- server/src/routes/diary.js
+++ server/src/routes/diary.js
@@ -1,23 +1,20 @@
+
 //다이어리 관련 API 작성
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
 
 
+// JWT 미들웨어
+const authToken = require("../middlewares/authToken");
+
 
 const diaryCtrl = require("../controllers/diaryCtrl");
 
 const storage = multer.memoryStorage(); //메모리에 파일 저장
 const upload = multer({storage: storage}); //multer 설정
-router.post("/", upload.single("image"), diaryCtrl.create );
-router.get('/:user_id/:date', diaryCtrl.getDiary);
+router.post("/", authToken, upload.single("image"), diaryCtrl.create );
+router.get('/:user_id/:date',authToken, diaryCtrl.getDiary);
 
-// JWT 미들웨어
-const authToken = require("../middlewares/authToken");
-
-// 라우터
-router.post("/login", authCtrl.login);
-router.post("/tokens", authCtrl.refresh)
-router.delete("/logout", authToken, authCtrl.logout);
 
 module.exports = router;
