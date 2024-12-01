1e76a3852b178baa00c78abce8c8627de30d7c94
PSO01 <rndrmrqkaqka@naver.com>
2024-12-01 17:49:54 +0900

    [FEAT] 일기작성 api요청 임시구현

--- client/src/pages/DiaryPage.jsx
+++ client/src/pages/DiaryPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import DateNavigation from "components/diary/DateNavigation";
 import ImageUploader from "components/diary/ImageUploader";
 import DiaryForm from "components/diary/DiaryForm";
@@ -8,6 +8,7 @@ import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { postDiary } from "api/diary";
 import { putEdit } from "api/diary";
+import UserContext from "contexts/UserContext";
 
 const DiaryPage = () => {
 
@@ -21,25 +22,32 @@ const DiaryPage = () => {
     useEffect(() => {
       const selectedDate = location.state?.selectedDate;
       const [year, month, day] = selectedDate.split("-");
-      setCurrentDate(`${year}년 ${month}월 ${day}일`);
-    }, [location.state]);
-    
+      const formattedDay = day.padStart(2, '0');
+       setCurrentDate(`${year}-${month}-${formattedDay}`); // YYYY-MM-DD 형식
+      }, [location.state]);
 
     // 이미지 선택 핸들러
     const handleImageUpload = (img, file) => {
       setSelectedImage(img);
     };
+
+    // 로그인된 사용자 아이디 가져오기
+    const { user } = useContext(UserContext);
   
     // 저장 버튼 클릭 핸들러
     const handleSave = async () => {
       try {
         const formData = new FormData();
-        formData.append('user_id', 'admin');
+        formData.append('user_id', user);
         formData.append('date', currentDate);
         formData.append('title', diaryContent.title);
         formData.append('content', diaryContent.content);
         if (selectedImage) {
           formData.append('image', selectedImage);
+        }
+
+        for (let pair of formData.entries()) {
+          console.log(pair[0] + ', ' + pair[1]);
         }
 
         const response = await postDiary(formData);
@@ -52,7 +60,6 @@ const DiaryPage = () => {
         console.log(error.response.data.message);
       }
     };
-
   
     // 수정 버튼 클릭 핸들러
     const handleEdit = () => {
@@ -61,24 +68,9 @@ const DiaryPage = () => {
     };
   
     // 수정 확인 버튼 클릭 핸들러
-    const handleConfirmEdit = async () => {
-      try{
-        const formData = new FormData();
-        formData.append('title', diaryContent.title);
-        formData.append('content', diaryContent.content);
-        if (selectedImage) {
-          formData.append('image', selectedImage);
-        }
-
-        const response = await putEdit(formData);
-          
-        if (response.status === 201){
-          setIsEditing(false);
-          setIsSaved(true);
-        }
-      } catch (error) {
-        console.log(error.response.data.message);
-      }
+    const handleConfirmEdit = () => {
+       setIsEditing(false);
+       setIsSaved(true);
     };
   
     // 이미지 취소 핸들러
