9c65b112c98b5af0e84fd03c334fb058a875585d
leeeeejieun <arjleun19@gmail.com>
2024-12-18 21:30:28 +0900

    [FEAT] 월별 감정 이모티콘 API 호출 코드 구현

---
+++ client/src/api/calendar.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+export const getMonthEmoji = async (userData) => {
+    const {user_id, date} = userData;
+    const response = await axios.get(`${SERVER_URL}/analysis/month/${user_id}/${date}`);
+    return response;
+}
