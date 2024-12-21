0014fb917ed7639dcc7a5f47db2104e0aee14d6f
leeeeejieun <arjleun19@gmail.com>
2024-12-21 12:41:52 +0900

    [FEAT] 연도와 관련된 함수 추가
    - 이전 연도, 다음 연도로 날짜를 업데이트 하는 함수 추가
    - 월 포맷 형식을 MM -> M 으로 변경

--- client/src/hooks/useCalender.js
+++ client/src/hooks/useCalender.js
@@ -1,6 +1,8 @@
 import {
     addMonths,
     subMonths,
+    addYears,
+    subYears,
     eachDayOfInterval,
     endOfMonth,
     endOfWeek,
@@ -13,8 +15,9 @@ import { useState } from "react";
 
 const useCalender = () => {
     const [currentDate, setCurrantDate] = useState(new Date());  // Sun Nov 17 2024 00:30:08 GMT+0900 (한국 표준시)과 같은 데이터 반환
-    const [currentMonth] = format(currentDate, "MM").split("-"); // 현재 월 반환
     const [selectedDate, setSelectedDate] = useState(format(new Date(), "yyyy-MM-d"));  // 사용자가 선택한 날짜를 저장(기본 값은 현재 날짜)
+    const currentMonth = format(currentDate, "M"); // 현재 월 반환
+    const currentYear = format(currentDate, "yyyy") // 현재 연도 반환
 
    const startCurrentMonth = startOfMonth(currentDate);        
    const endCurrentMonth = endOfMonth(currentDate);            
@@ -37,6 +40,15 @@ const useCalender = () => {
         setCurrantDate((prevDate) => addMonths(prevDate, 1));
     }
 
+    // 이전 연도로 이동
+    const handlePrevYear = () => {
+        setCurrantDate((prevDate) => subYears(prevDate, 1));
+    }
+
+    // 다음 연도로 이동
+    const handleNextYear = () => {
+        setCurrantDate((prevDate) => addYears(prevDate, 1));
+    }
 
     // 특정 날짜 선택
     const handleSelectDate = (date) => {
@@ -45,21 +57,24 @@ const useCalender = () => {
 
     // 각 날짜에 대한 정보 반환
     const daysInMonth = days.map((day) => {
-        const isCurrentMonth = format(day, "MM") === currentMonth // 주어진 날짜가 해당 월에 속하는지 확인
+        const isCurrentMonth = format(day, "M") === currentMonth // 주어진 날짜가 해당 월에 속하는지 확인
         return {
-        date: format(day, "yyyy-MM-d"),         
+        date: format(day, "yyyy-M-d"),         
         day: format(day, "d"),              
         dayIndexOfWeek: getDay(day),    
         isInMonth: isCurrentMonth, 
     }});
 
    return {
-        currentDate: format(currentDate, "yyyy-MM-d"),
+        currentDate: format(currentDate, "yyyy-M-d"),
+        currentYear,
         currentMonth,
         daysInMonth,
         dispatch: {
             handlePrevMonth,
             handleNextMonth,
+            handlePrevYear,
+            handleNextYear,
         },
         selectedDate: {
             date: selectedDate,
