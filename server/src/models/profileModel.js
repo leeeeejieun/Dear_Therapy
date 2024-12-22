12628bd21d1b6cd829a26ad4994b22fd6c3012b0
leeeeejieun <arjleun19@gmail.com>
2024-12-22 19:59:35 +0900

    [FEAT] 프로필 정보 조회 API 모델 추가

--- server/src/models/profileModel.js
+++ server/src/models/profileModel.js
@@ -23,6 +23,19 @@ class Profile {
 
         return {code: 200};
     };
+
+    async getProfile() {
+        const {user_id} = this.body;
+
+        if(!user_id) {
+            return {code: 400, message: "잘못된 형태의 데이터 입니다."}; 
+        }
+
+        const profile = await ProfileStorage.getProfile(user_id);
+        const image = profile.profile_image ? await s3Utils.getImage(profile.profile_image) : null;
+        
+        return {code: 200, data: {name: profile.user_name, image: image}};
+    }
 }
 
 module.exports = Profile;
--- server/src/models/profileStorage.js
+++ server/src/models/profileStorage.js
@@ -11,6 +11,12 @@ class ProfileStorage {
         const query = "UPDATE User SET profile_image = ? WHERE user_id =?;";
         await db.connection(query, [imageUrl, user_id]);
     }
+    
+    static async getProfile(user_id) {
+        const query = "SELECT user_name, profile_image FROM User WHERE user_id = ?;";  
+        const response = await db.connection(query, [user_id]);
+        return response;
+    }
 };
 
 module.exports = ProfileStorage;
