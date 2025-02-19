f744f055f0074d3ab09ac14c0e21ec3e2fd06422
leeeeejieun <arjleun19@gmail.com>
2025-02-19 23:37:01 +0900

    [FEAT] 감정 분석 결과 페이지 구현

--- client/src/pages/EmotionAnalysisPage.jsx
+++ client/src/pages/EmotionAnalysisPage.jsx
@@ -1,35 +1,36 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation  } from 'react-router-dom';
+import BottomNavigation from 'components/common/BottomNavigation';
+import AnalysisResult from 'components/analysis/AnalysisResult';
 import styled from 'styled-components';
 
 const EmotionAnalysisPage = () => {
   const location = useLocation();
-  const { user_id, date, analysisResult } = location.state || {};
+  const params = new URLSearchParams(location.search);
+  const date = params.get("date");
 
   return (
-    <PageContainer>
+    <AnalysisContainer>
       <Title>테피의 추천</Title>
-    </PageContainer>
+      <AnalysisResult date={date}/>
+      <BottomNavigation />
+    </AnalysisContainer>
   );
 }
 
 export default EmotionAnalysisPage;
 
-const PageContainer = styled.div`
+const AnalysisContainer = styled.div`
   display: flex;
   flex-direction: column;
-  height: 50vh;
-  background-color: #FFFF;
-  outline: 2px dashed #ccc;
-  border-radius: 10px;
-  margin: 20px 20px;
+  justify-content: space-between;
+  height: 100vh;
 `;
 
 const Title = styled.h3`
-  position: absolute;
-  top: 30px;
-  left: 50%;
-  transform: translate(-50%);
+  position: relative;
+  top: 25px;
+  text-align: center;
   font-weight: bold;
-  font-family: '빵구니맘';
-  font-size: 24px;
+  font-family: "빵구니맘";
+  font-size: 33px;
 `;
